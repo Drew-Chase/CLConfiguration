600b80f49c44d716876f28acd766d6cc0f044e22
dcmanproductions <dcmanproductions@gmail.com>
2022-07-11 12:02:14 -0400

    Removed unnecessary access to constructor

--- Object/Config.cs
+++ Object/Config.cs
@@ -1,63 +1,57 @@
-﻿using ChaseLabs.CLConfiguration.Interfaces;
-using ChaseLabs.CLConfiguration.List;
+﻿using ChaseLabs.CLConfiguration.List;
 
-namespace ChaseLabs.CLConfiguration.Object
+namespace ChaseLabs.CLConfiguration.Object;
+
+/// <summary>
+/// <para>Author: Drew Chase</para>
+/// <para>Company: Chase Labs</para>
+/// </summary>
+public class Config
 {
+    #region Fields
+
+    public ConfigManager Manager;
+
+    private dynamic _value;
+
+    #endregion Fields
+
+    #region Internal Constructors
+
     /// <summary>
-    /// <para> Author: Drew Chase </para>
-    /// <para> Company: Chase Labs </para>
+    /// Creates a Config Object with Key and Value
     /// </summary>
-    public class Config : IConfig
+    /// <param name="_key"></param>
+    /// <param name="_value"></param>
+    /// <param name="_manager"></param>
+    internal Config(string _key, dynamic _value, ConfigManager _manager)
     {
-        #region Public Fields
-
-        public ConfigManager Manager;
-
-        #endregion Public Fields
-
-        #region Private Fields
-
-        private dynamic _value;
-
-        #endregion Private Fields
-
-        #region Public Constructors
-
-        /// <summary>
-        /// Creates a Config Object with Key and Value
-        /// </summary>
-        /// <param name="_key">     </param>
-        /// <param name="_value">   </param>
-        /// <param name="_manager"> </param>
-        public Config(string _key, dynamic _value, ConfigManager _manager)
-        {
-            Key = _key;
-            this._value = _value;
-            Manager = _manager;
-        }
-
-        #endregion Public Constructors
-
-        #region Public Properties
-
-        /// <summary>
-        /// Returns the Configs Key
-        /// </summary>
-        public string Key { get; }
-
-        /// <summary>
-        /// Returns the Configs Value
-        /// </summary>
-        public dynamic Value
-        {
-            get => _value;
-            set
-            {
-                _value = value;
-                Manager.outdated = true;
-            }
-        }
-
-        #endregion Public Properties
+        Key = _key;
+        this._value = _value;
+        Manager = _manager;
     }
+
+    #endregion Internal Constructors
+
+    #region Properties
+
+    /// <summary>
+    /// Returns the Configs Key
+    /// </summary>
+    public string Key { get; }
+
+    /// <summary>
+    /// Returns the Configs Value
+    /// </summary>
+    public dynamic Value
+    {
+        get => _value;
+        set
+        {
+            _value = value;
+            Manager.outdated = true;
+        }
+    }
+
+    #endregion Properties
 }
