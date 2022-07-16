c70a8a800b79a1fa3f324e2c8f365957bf4580be
dcmanproductions <dcmanproductions@gmail.com>
2022-07-15 20:20:12 -0400

    Added Low Memory Config

---
+++ CLConfiguration/LowMemoryConfigManager.cs
@@ -0,0 +1,90 @@
+﻿using ChaseLabs.CLConfiguration.Object;
+using System;
+using System.IO;
+
+namespace ChaseLabs.CLConfiguration;
+
+public class LowMemoryConfigManager
+{
+    #region Private Fields
+
+    private string _path = string.Empty;
+
+    private bool dirty = false;
+
+    #endregion Private Fields
+
+    #region Public Constructors
+
+    /// <summary>
+    /// Initializes the Config Manager with a File Path
+    /// </summary>
+    /// <param name="name">
+    /// Config File Name not including extension. <br /> Ex: <i> config_file <b> <u> NOT </u></b>
+    /// config_file.json </i>
+    /// </param>
+    /// <param name="directory"> Path to Config Items </param>
+    /// <exception cref="ArgumentException" />
+    public LowMemoryConfigManager(string name, string directory)
+    {
+        EncryptionPassword = directory;
+        Path = Directory.CreateDirectory(directory).FullName;
+        Name = name;
+    }
+
+    #endregion Public Constructors
+
+    #region Public Properties
+
+    /// <summary>
+    /// The Password used to Encrypt the Config File
+    /// </summary>
+    public string EncryptionPassword { get; private set; }
+
+    /// <summary>
+    /// Sets the Name of the Config
+    /// <para> Good if You have multiple Configs </para>
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// Returns the Directory that holds all of the data files
+    /// </summary>
+    public string Path { get; init; }
+
+    /// <summary>
+    /// If true the Config File will be encrypted.
+    /// </summary>
+    public bool UseEncryption { get; private set; }
+
+    #endregion Public Properties
+
+    #region Public Methods
+
+    /// <summary>
+    /// Gets an existing config item or creates one with value
+    /// </summary>
+    /// <param name="key"> </param>
+    /// <param name="default_value"> </param>
+    /// <param name="encrypt_output"> if the value should be encrypted </param>
+    /// <returns> </returns>
+    public LowMemoryConfig GetOrCreate(string key, dynamic default_value, bool encrypt_output = false)
+    {
+        return new(key, default_value, this, encrypt_output);
+    }
+
+    /// <summary>
+    /// Removes Config Input by Config Key
+    /// </summary>
+    /// <param name="key"> </param>
+    public void Remove(string key)
+    {
+        string file = System.IO.Path.Combine(Path, $"{key}.dat");
+        if (File.Exists(file))
+        {
+            File.Delete(file);
+        }
+    }
+
+    #endregion Public Methods
+}
---
+++ CLConfiguration/Object/LowMemoryConfig.cs
@@ -0,0 +1,92 @@
+﻿using ChaseLabs.Math;
+using System;
+using System.IO;
+
+namespace ChaseLabs.CLConfiguration.Object;
+
+/// <summary>
+/// <para> Author: Drew Chase </para>
+/// <para> Company: Chase Labs </para>
+/// </summary>
+public class LowMemoryConfig
+{
+    #region Private Fields
+
+    private bool Encrypt;
+    private LowMemoryConfigManager Manager;
+
+    #endregion Private Fields
+
+    #region Internal Constructors
+
+    /// <summary>
+    /// Creates a Config Object with Key and Value
+    /// </summary>
+    /// <param name="_key"> </param>
+    /// <param name="_value"> </param>
+    /// <param name="_manager"> </param>
+    /// <param name="encrypt_output"> </param>
+    internal LowMemoryConfig(string _key, dynamic _value, LowMemoryConfigManager _manager, bool encrypt_output)
+    {
+        Manager = _manager;
+        Encrypt = encrypt_output;
+        Key = _key;
+
+        string path = Path.Combine(Manager.Path, $"{Key}.dat");
+        if (!File.Exists(path)) Write(_value);
+
+    }
+
+    #endregion Internal Constructors
+
+    #region Public Properties
+
+    /// <summary>
+    /// Returns the Configs Key
+    /// </summary>
+    public string Key { get; }
+
+    /// <summary>
+    /// Returns the Configs Value
+    /// </summary>
+    public dynamic Value
+    {
+        get => Encrypt ? AESMath.DecryptStringAES(Read(), Manager.EncryptionPassword) : Read();
+        set
+        {
+            Write(Encrypt ? AESMath.EncryptStringAES(value, Manager.EncryptionPassword) : value);
+        }
+    }
+
+    #endregion Public Properties
+
+    #region Private Methods
+
+    private dynamic Read()
+    {
+        string path = Path.Combine(Manager.Path, $"{Key}.dat");
+        using BinaryReader reader = new(new FileStream(path, FileMode.Open, FileAccess.Read));
+        string bin = reader.ReadString();
+        string type = bin.Substring(1, bin.IndexOf(')') - 1);
+        GC.Collect();
+        return Convert.ChangeType(bin.Replace($"({type})", ""), Type.GetType(type));
+    }
+
+    private void Write(dynamic value)
+    {
+        string path = Path.Combine(Manager.Path, $"{Key}.dat");
+        using BinaryWriter writer = new(new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write));
+        writer.Write($"({value.GetType()}){value}");
+        GC.Collect();
+    }
+    /// <summary>
+    /// Returns the Value
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+        return Value.ToString();
+    }
+
+    #endregion Private Methods
+}
